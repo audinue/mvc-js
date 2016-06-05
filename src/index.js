355212fb1d09331693d64a0d1979ca6c1301181f
Audi Nugraha <audinue@gmail.com>
2016-06-06 04:49:42 +0700

    Update binding-js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+exports.convert = require('binding-js').convert
+
+exports.register = require('binding-js').register
+
+exports.bind = require('binding-js').bind
+
+exports.delegate = require('delegate-js')
